7230f901cde4aa36e1892f9fa7f3c2619cd0106e
Admin <admin@email.com>
2024-07-03 13:54:04 +0800

    Make The Contact Form Work

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from getpass import getpass
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import getpass
 
 from datetime import datetime
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 # Get the data of Blogs
 data = requests.get("https://api.npoint.io/548bb22abe9204c52867").json()
-
 
 
 @app.route("/")
@@ -18,20 +17,24 @@ def get_all_posts():
     return render_template("index.html", data=data)
 
 
-
 @app.route("/about")
 def about_page():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact_page():
+    if request.method == "POST":
+        name = request.form['name']
+        print(name)
     return render_template("contact.html")
+
 
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
     return render_template("post.html", post=data[post_id - 1])
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=4008)
